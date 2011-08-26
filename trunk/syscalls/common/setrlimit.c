938fb3d23b454a56c291426d717e43957eca45bb
taviso <taviso@6419abc6-8ce4-11de-927e-ab586cd9c9de>
2011-08-26 17:42:05 +0000

    Add the new prlimit64 and fanotify syscalls.

--- trunk/syscalls/common/setrlimit.c
+++ trunk/syscalls/common/setrlimit.c
@@ -19,12 +19,12 @@
 // int setrlimit(int resource, const struct rlimit *rlim);
 SYSFUZZ(setrlimit, __NR_setrlimit, SYS_NONE, CLONE_FORK, 0)
 {
-	gpointer    rlim;
-	glong       retcode;
+    gpointer    rlim;
+    glong       retcode;
 
-	retcode = spawn_syscall_lwp(this, NULL, __NR_setrlimit,                                 // int
-	                            typelib_get_integer_range(0, RLIM_NLIMITS),                 // int resource
-	                            typelib_get_buffer(&rlim, PAGE_SIZE));                      // struct rlimit *rlim
+    retcode = spawn_syscall_lwp(this, NULL, __NR_setrlimit,                                 // int
+                                typelib_get_integer_range(0, RLIM_NLIMITS),                 // int resource
+                                typelib_get_buffer(&rlim, PAGE_SIZE));                      // struct rlimit *rlim
 
     typelib_clear_buffer(rlim);
     return retcode;
